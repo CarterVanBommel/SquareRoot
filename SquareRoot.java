62cb423331d698e866bd2cec9c041ca49b4627fb
Carter VanBommel <71711972+CarterVanBommel@users.noreply.github.com>
2020-10-08 10:19:20 -0400

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,36 +1,49 @@
 /*
- * Programmer:
- * Date:
- * Purpose: 
+ * Programmer: Carter Van Bommel
+ * Date: October 8, 2020
+ * Purpose: To calculate the square root of a positive number
  */
 
+// Imports DecimalFormat and Scanner 
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class SquareRoot {
+	
     public static void main (String [] args) {
+    	
         // Create the input scanner
         Scanner input = new Scanner(System.in);
 
+        
         // Ask the user for the diameter
         System.err.print("Enter a number to square root: ");
         double radicand = input.nextDouble();
 
+        
         // Check that the input is not negative
-        // TODO: Change this part of the program to repeatedly error check
-        if (radicand < 0) {
+        while (radicand <= 0) {
             System.err.println("Invalid input!");
-            System.exit(1);
+            System.err.print("Enter a number to square root: ");
+            radicand = input.nextDouble(); 
         }
 
+        
         // Calculate the circumference
         double root = Math.sqrt(radicand);
 
+        
         // Create a decimal formatter
         DecimalFormat formatter = new DecimalFormat("#,###.##");
 
+        
         // Print the results
         System.out.println("The square root of " + formatter.format(radicand) + 
-                " is " + formatter.format(root) + ".");
+        		" is " + formatter.format(root) + ".");
+        
+        
+        input.close();
+        
     }
+    
 }
